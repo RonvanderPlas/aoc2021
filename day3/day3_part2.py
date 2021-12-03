40026afd548e4c430a410782f54953afd97c161c
Ron van der Plas <r.vanderplas@homewizard.nl>
2021-12-03 09:43:02 +0100

    Day3 Part2

--- day3/day3_part2.py
+++ day3/day3_part2.py
@@ -1,12 +1,12 @@
 def open_file_as_list():
-    f = open('temp.txt')
+    f = open('day3.txt')
     lines = f.readlines()
     file_list = list(lines)
     return file_list
 
 def most_common(lst):
     ones = lst.count('1')
-    zeros = lst.count('1')
+    zeros = lst.count('0')
     if ones >= zeros :
         return '1'
     else:
@@ -14,8 +14,8 @@ def most_common(lst):
 
 def least_common(lst):
     ones = lst.count('1')
-    zeros = lst.count('1')
-    if zeros >= ones :
+    zeros = lst.count('0')
+    if zeros <= ones :
         return '0'
     else:
         return '1'
@@ -26,22 +26,23 @@ def binlist_to_decimal(lst):
         rv = (rv << 1) | int(bit)
     return rv
 
-def get_generator(lst, bit, mask):
-    return [ x for x in lst if lst[bit] == mask ]
-
 f = open_file_as_list()
 bit_length = len(f[0].strip())
 generator = f
-scrubber = f 
+generator_value = 0
 
-print(int(most_common([line.strip()[0] for line in f]))^1)
-print(least_common([line.strip()[0] for line in f]))
+scrubber = f
+scrubber_value = 0
+
 for i in range(bit_length):
-    gen_mask = int(most_common([line.strip()[i] for line in f]))^1
-    print([ x for x in generator if generator[i] == gen_mask ])
+    gen_mask = int(most_common([line.strip()[i] for line in generator]))
+    generator = [ x.strip() for x in generator if int(x[i]) == gen_mask]
+    if len(generator) == 1:
+        generator_value = binlist_to_decimal(generator[0])
 
-# gamma = binlist_to_decimal(gamma_lst)
-# epsilon = gamma ^ ((2**bit_length) - 1)
+    scrub_mask = int(least_common([line.strip()[i] for line in scrubber]))
+    scrubber = [ x.strip() for x in scrubber if int(x[i]) == scrub_mask]
+    if len(scrubber) == 1:
+        srubber_value = binlist_to_decimal(scrubber[0])
 
-print(f'Gamma: {gamma} Epsilson {epsilon} Multiplied: {gamma * epsilon}')
-    
+print(f'Generator: {generator_value} Scrubber: {srubber_value} Multiplied: {srubber_value*generator_value}')
